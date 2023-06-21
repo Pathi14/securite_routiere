b6005ddfca839d1380c2d3833a626b2adb1e52f0
pathi14 <pathipayong@gmail.com>
2023-06-21 12:26:39 +0100

    Ajout element IU

---
+++ src/main/java/com/circulation_routiere/securite_routiere/SecuriteRoutiereApplication.java
@@ -0,0 +1,13 @@
+package com.circulation_routiere.securite_routiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuriteRoutiereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuriteRoutiereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circulation_routiere/securite_routiere/SecuriteRoutiereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circulation_routiere.securite_routiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuriteRoutiereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
